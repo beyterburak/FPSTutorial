1871e3b697aa71515b237f50ad33999d7d0cecad
BURAK\beyte <beyterburak@gmail.com>
2024-02-06 21:27:32 +0300

    added: Bullet Impact Effect

--- FPSTutorial/Assets/Scripts/Bullet.cs
+++ FPSTutorial/Assets/Scripts/Bullet.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision objectWeHit)
     {
-        if (collision.gameObject.CompareTag("Target"))
+        if (objectWeHit.gameObject.CompareTag("Target"))
         {
-            print("hit " + collision.gameObject.name + " !");
+            print("hit " + objectWeHit.gameObject.name + " !");
+
+            CreateBulletImpactEffect(objectWeHit);
+
             Destroy(gameObject);
         }
 
-        if (collision.gameObject.CompareTag("Wall"))
+        if (objectWeHit.gameObject.CompareTag("Wall"))
         {
             print("hit a wall");
+
+            CreateBulletImpactEffect(objectWeHit);
+
             Destroy(gameObject);
         }
+    }
+
+    void CreateBulletImpactEffect(Collision objectWeHit)
+    {
+        ContactPoint contact = objectWeHit.contacts[0];
+
+        GameObject hole = Instantiate(
+            GlobalReferences.Instance.bulletImpactEffectPrefab,
+            contact.point,
+            Quaternion.LookRotation(contact.normal)
+            );
+
+        hole.transform.SetParent(objectWeHit.gameObject.transform);
     }
 }
